601eef474e5f2037804997e3ffc7e6e25025c9fc
santos-nathalia86 <133174209+santos-nathalia86@users.noreply.github.com>
2023-06-09 16:10:19 -0300

    Add files via upload

---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+height: 150px;
+text-align: center;
+background: pink;
+`;
+
+export const Header = styled.h1``;
+export const Title = styled.div`
+padding: 40px;
+text-transform: uppercase;
+color: #000;
+`;
